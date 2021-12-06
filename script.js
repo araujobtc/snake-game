cad0668af1c605ec8146f7e4a2761677b050d4cd
araujobtc <isabelletecn@gmail.com>
2021-12-06 16:02:15 -0300

    motion control and food
    insertion of movement control and food for the snake.

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d");
-let box = 32;   /* 32 pixels cada quadrado */
-let snake = [];
+let box = 32;   // 32 pixels cada quadrado
+let direction = "right"
+let snake = []; //criar cobra como lista
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
-let direction = "right"
-
-function createBG(){
-    context.fillStyle = "darkgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box); /*desenha x, y, altura, largura*/
+let positionFood = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
+// Criação de Background e Cobra
+function createBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box); //desenha x, y, altura, largura
+}
+
+// Criação da Cobra
 function createSnake(){
     for(i=0; i<snake.length; i++){
-        context.fillStyle = "brown";
+        context.fillStyle = "rgb(128, 74, 52)";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-let snakeX;
 
+// Criação da Comida
+function drawFood(){
+    context.fillStyle = "rgb(233, 47, 47)";
+    context.fillRect(positionFood.x, positionFood.y, box, box);
+}
+
+// controle de movimento
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction !="right") direction = "left";
+    if(event.keyCode == 38 && direction !="down") direction = "up";
+    if(event.keyCode == 39 && direction !="left") direction = "right";
+    if(event.keyCode == 40 && direction !="up") direction = "down";
+}
+
+// Iniciação do jogo e definindo movimentos
 function startGame(){
+
+// controle de movimento
+if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    
     createBG();
     createSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-
+    
+// movimentos
     if(direction == "right") snakeX +=box;
     if(direction == "left") snakeX -=box;
-    if(direction == "up") snakeY +=box;
-    if(direction == "down") snakeY -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
 
     snake.pop();
 
@@ -45,3 +76,4 @@ function startGame(){
 }
 
 let game = setInterval(startGame, 100)
+

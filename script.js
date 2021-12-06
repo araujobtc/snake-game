f390cb1ee8705a818354b83628b904cd836ce1f1
araujobtc <isabelletecn@gmail.com>
2021-12-06 16:32:24 -0300

    game over, style and food

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d");
 let box = 32;   // 32 pixels cada quadrado
+let interval = 130;
 let direction = "right"
 let snake = []; //criar cobra como lista
 snake[0] = {
@@ -46,11 +47,17 @@ function update(event){
 function startGame(){
 
 // controle de movimento
-if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
-if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
-if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
     
     createBG();
     createSnake();
@@ -65,7 +72,14 @@ if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
     if(direction == "up") snakeY -=box;
     if(direction == "down") snakeY +=box;
 
-    snake.pop();
+    if(snakeX != positionFood.x || snakeY != positionFood.y){
+        snake.pop();
+    }
+    else{
+        positionFood.x = Math.floor(Math.random() * 15 + 1) * box;
+        positionFood.y = Math.floor(Math.random() * 15 + 1) * box;
+
+    }
 
     let newHead = {
         x: snakeX, 
@@ -75,5 +89,5 @@ if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
     snake.unshift(newHead)
 }
 
-let game = setInterval(startGame, 100)
+let game = setInterval(startGame, interval)
 

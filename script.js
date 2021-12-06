37a53953f6c078ec422f67716d01c4746823ea66
araujobtc <isabelletecn@gmail.com>
2021-12-06 14:45:24 -0300

    movements
    insertion of movements

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;   /* 32 pixels cada quadrado */
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
 
+let direction = "right"
 
 function createBG(){
     context.fillStyle = "darkgreen";
@@ -20,6 +20,28 @@ function createSnake(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+let snakeX;
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX +=box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY +=box;
+    if(direction == "down") snakeY -=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX, 
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let game = setInterval(startGame, 100)
